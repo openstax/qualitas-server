b639be9ab64d13f96506c279249401b1521d40d6
m1yag1 <mike.arbelaez@rice.edu>
2018-08-17 16:07:54 -0500

    removing unusued imports from home view

--- qualitas/home/views.py
+++ qualitas/home/views.py
@@ -1,15 +1,9 @@
 import logging
 
 from flask import (Blueprint,
-                   render_template,
-                   request,
-                   redirect,
-                   url_for, flash)
+                   render_template)
 
-from qualitas.lib.github_export import export
-from qualitas.utils import render_csv
 
-logging.basicConfig(level=logging.INFO)
 LOGS = logging.getLogger(__name__)
 
 home = Blueprint('home',
