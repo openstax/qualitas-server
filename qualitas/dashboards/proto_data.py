34dab32094d7a38c64bb4a9fc6f841a30636f2d5
m1yag1 <mike.arbelaez@rice.edu>
2018-08-20 08:31:28 -0500

    Added materialized prototype of the dashboard
    - added title to PR Commit Exporter template
    - did some old comment cleanup
    - cleaned up the base template
    - added prototype data file

---
+++ qualitas/dashboards/proto_data.py
@@ -0,0 +1,56 @@
+quick_links = [
+    {
+        'label': 'oer.exports releases',
+        'url': 'https://github.com/Connexions/oer.exports/releases'
+    },
+    {
+        'label': "RhaptosPrint commits on 'production' branch",
+        'url': 'https://github.com/Rhaptos/Products.RhaptosPrint/commits/production'
+    },
+    {
+        'label': 'dist.rhaptos.org (legacy/plone eggs)',
+        'url': 'http://dist.rhaptos.org/'
+    },
+    {
+        'label': 'packages.cnx.org/js-builds (webview builds)',
+        'url': 'https://packages.cnx.org/js-builds/'
+    }
+]
+
+cnx_server_files = [
+    {
+        'server_name': 'devb',
+        'server_url': 'https://devb.cnx.org',
+        'history_url': 'https://devb.cnx.org/history.txt',
+        'version_url': 'https://devb.cnx.org/version.txt',
+        'python_version_url': 'https://devb.cnx.org/python-version.txt'
+    },
+    {
+        'server_name': 'qa',
+        'server_url': 'https://qa.cnx.org',
+        'history_url': 'https://qa.cnx.org/history.txt',
+        'version_url': 'https://qa.cnx.org/version.txt',
+        'python_version_url': 'https://qa.cnx.org/python-version.txt'
+    },
+    {
+        'server_name': 'staging',
+        'server_url': 'https://staging.cnx.org',
+        'history_url': 'https://staging.cnx.org/history.txt',
+        'version_url': 'https://staging.cnx.org/version.txt',
+        'python_version_url': 'https://staging.cnx.org/python-version.txt'
+    },
+    {
+        'server_name': 'production',
+        'server_url': 'https://cnx.org',
+        'history_url': 'https://cnx.org/history.txt',
+        'version_url': 'https://cnx.org/version.txt',
+        'python_version_url': 'https://cnx.org/python-version.txt'
+    },
+    {
+        'server_name': 'content01',
+        'server_url': 'https://content01.cnx.org',
+        'history_url': 'https://content01.cnx.org/history.txt',
+        'version_url': 'https://content01.cnx.org/version.txt',
+        'python_version_url': 'https://content01.cnx.org/python-version.txt'
+    },
+]
