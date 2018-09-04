d12804bcebcecfa535a8797b282bd505e910f555
m1yag1 <mike.arbelaez@rice.edu>
2018-09-04 08:25:48 -0500

    Added support for multiple repos in pr commit export tool
    * renamed fields on PullRequestExportForm to allow for cloning.
    * took the common fields for a PR lookup and made them into a fieldset
    * added clone functionality to the pr-export form fields to allow support for many repositories.
    * added remove button so that fields could be removed.
    * changed layout of the form so fields sat on one line.
    * updated view so that it could support 1 or more repos.
    * added fieldset border-style to none in style.css

--- qualitas/tools/views.py
+++ qualitas/tools/views.py
@@ -10,6 +10,7 @@ from qualitas.tools.forms import PullRequestExportForm
 from qualitas.lib.github_export import export
 from qualitas.utils import render_csv
 
+logging.basicConfig(level=logging.DEBUG)
 LOGS = logging.getLogger(__name__)
 
 tools = Blueprint('tools',
@@ -20,25 +21,51 @@ tools = Blueprint('tools',
 
 @tools.route('/pr-export', methods=['GET', 'POST'])
 def pull_request_export():
-    form = PullRequestExportForm(request.form)
-    if form.validate_on_submit():
-        LOGS.info('Pull Request Form validated correctly')
-        repo_name = form.repo.data
-        base = form.base.data
-        head = form.head.data
+    form = PullRequestExportForm()
 
-        pr_commits = export.get_pr_commit_data(repo_name, base, head)
+    if request.method == 'POST':
+        form_data = dict(request.form)
+        # Remove CSRF Token
+        form_data.pop('csrf_token')
+        LOGS.debug(form_data)
+
+        # Ensure the number of fields is a multiple of 3
+        if len(form_data) % 3 != 0:
+            flash('Incorrect number of entries per repository')
+            return redirect(url_for('tools.pull_request_export'))
+
+        LOGS.info('The POSTED form has the correct number of fields')
+
+        # Determine the number of repos
+        repo_num = int(len(form_data) / 3)
+
+        LOGS.info(f'Total Repos to check are {repo_num}')
+
+        pr_commits = []
+
+        for n in range(1, repo_num + 1):
+            repo_name = form_data[f'repo_{n}'][0]
+            base = form_data[f'base_{n}'][0]
+            head = form_data[f'head_{n}'][0]
+            LOGS.info(f'Currently processing {repo_name} b{base} h{head}')
+            commits = export.get_pr_commit_data(repo_name,
+                                                base,
+                                                head)
+            if commits:
+                pr_commits.extend(commits)
+            else:
+                LOGS.info(f'No PR Commits found for {repo_name}')
 
         if pr_commits:
             LOGS.info('There were pr {} commits found'.format(len(pr_commits)))
             fieldnames = pr_commits[0].keys()
 
             return render_csv(
-                fieldnames, pr_commits, '{}-{}-{}'.format(repo_name.split('/')[1], base, head))
+                fieldnames, pr_commits, 'pr-commits')
         else:
             LOGS.info('Something went wrong or no results were found')
             flash('There was a problem trying to find pull request commits. '
-                  'Ensure you selected the proper repository')
+                  'Ensure you filled out all fields correctly')
             return redirect(url_for('tools.pull_request_export'))
 
     return render_template('pr_export.html', form=form)
