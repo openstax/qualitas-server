7f5267f09b3d0c541f2bba17402ea502a02be10a
m1yag1 <mike.arbelaez@rice.edu>
2018-10-13 23:28:21 -0500

    Add alembic migrations (#19)
    * update requirements for alembic and dependencies
    
    * update docker tag when pushing to heroku
    
    * have travis run docker-compose w/ postgres 10.5

--- instance/conf.py
+++ instance/conf.py
@@ -11,13 +11,9 @@ from qualitas.utils import make_database_url
 # MAIN FLASK APPLICATION
 DEBUG = os.environ.get('FLASK_DEBUG', False)
 SECRET_KEY = os.environ.get('SESSION_SECRET', str(uuid.uuid4()))
-
-# REDIS
-# SESSION_TYPE = 'redis'
-# REDIS_URL = os.environ.get('REDISTOGO_URL', None)
-# SESSION_REDIS = redis.from_url(REDIS_URL)
+SQLALCHEMY_DATABASE_URI = make_database_url()
+SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # GITHUB
-
 GITHUB_USER = os.environ.get('GITHUB_USER', None)
 GITHUB_PASSWORD = os.environ.get('GITHUB_PASSWORD', None)
