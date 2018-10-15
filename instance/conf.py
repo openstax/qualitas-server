1203386082dafaa703e003311b52e289f4b8fbf6
m1yag1 <mike.arbelaez@rice.edu>
2018-10-14 23:43:25 -0500

    added server_name to config and preferred_url_scheme (#22)

--- instance/conf.py
+++ instance/conf.py
@@ -11,6 +11,8 @@ DEBUG = os.environ.get('FLASK_DEBUG', False)
 SECRET_KEY = os.environ.get('SESSION_SECRET', str(uuid.uuid4()))
 SQLALCHEMY_DATABASE_URI = make_database_url()
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+SERVER_NAME = os.environ.get('FLASK_SERVER_NAME', None)
+PREFERRED_URL_SCHEME = os.environ.get('FLASK_PREFERRED_URL_SCHEME', 'http')
 
 # GITHUB
 GITHUB_USER = os.environ.get('GITHUB_USER', None)
