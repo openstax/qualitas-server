040f2a5bdeebc7789cff062f837ceb5e66e42755
m1yag1 <mike.arbelaez@rice.edu>
2019-04-12 16:29:09 -0500

    Updated app to use ghzh-clients library
    * Deleted GitHubClient code
    * Deleted ZenHubClient code
    * Moved the GitHubClient and ZenHubClient to the ghzh-clients library.
    * Moved client code located in `__init__.py` to `base.py` files.

---
+++ qualitas/ext/flask_zenhub/base.py
@@ -0,0 +1,27 @@
+from ghzh import ZenHubClient
+
+
+class ZenHub(object):
+    """A Flask extension that adds ZenHub functionality to our core app."""
+
+    def __init__(self, app=None, client_class=ZenHubClient):
+        self.app = app
+        self.client = client_class
+        self.token = None
+
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        self.app = app
+        app.zenhub = self
+        app.extensions['zenhub'] = self
+
+        app.config.setdefault('ZENHUB_TOKEN')
+
+        self.token = app.config['ZENHUB_TOKEN']
+
+        self.client = self.init_client()
+
+    def init_client(self):
+        return self.client(token=self.token)
