50f0de68a40a3085f7bbd3640d66ab92f9df3c65
m1yag1 <mike.arbelaez@rice.edu>
2019-05-08 16:35:59 -0500

    reorder params so they make more congnitive sense

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -7,9 +7,9 @@ from qualitas.utils import make_database_url
 URL_TEMPLATE = 'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'
 
 
-@pytest.mark.parametrize('url_template,db_name,db_user,db_password,db_host,db_port',
-                         [(URL_TEMPLATE, 'pizza', 'leonardo', 'cowabunga', '127.0.0.1', 5432)])
-def test_make_database_url_heroku(url_template, db_name, db_user, db_password, db_host, db_port):
+@pytest.mark.parametrize('url_template,db_user,db_password,db_host,db_port,db_name',
+                         [(URL_TEMPLATE, 'leonardo', 'cowabunga', '127.0.0.1', 5432, 'pizza_db')])
+def test_make_database_url_heroku(url_template, db_user, db_password, db_host, db_port, db_name):
     test_url = url_template.format(db_user, db_password, db_host, db_port, db_name)
 
     url = make_database_url()
@@ -21,9 +21,9 @@ def test_make_database_url_heroku(url_template, db_name, db_user, db_password, d
     assert url == test_url
 
 
-@pytest.mark.parametrize('url_template,db_name,db_user,db_password,db_host,db_port',
-                         [(URL_TEMPLATE, 'tests', 'postgres', '', 'db', 5432)])
-def test_make_database_url_default(url_template, db_name, db_user, db_password, db_host, db_port):
+@pytest.mark.parametrize('url_template,db_user,db_password,db_host,db_port,db_name',
+                         [(URL_TEMPLATE, 'postgres', '', 'db', 5432, 'tests')])
+def test_make_database_url_default(url_template, db_user, db_password, db_host, db_port, db_name):
 
     os.environ['DATABASE_URL'] = ''
 
