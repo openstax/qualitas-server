2abad41f9bc5c76c09671286193a22d1815b96fa
m1yag1 <mike.arbelaez@rice.edu>
2019-12-06 20:49:15 -0600

    fixed test_github_v4_call by quoting login org string

--- tests/integration/test_github.py
+++ tests/integration/test_github.py
@@ -5,7 +5,7 @@ def test_github_v4_call_api():
     gh = GitHubV4()
     result = gh.call_api("""\
 query {
-    organization(login: openstax) {
+    organization(login: "openstax") {
         login
     }
 }""")
