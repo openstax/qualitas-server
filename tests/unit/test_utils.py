cb2b2e0373929f07ba0e755dad7d0af2b2e33c4e
m1yag1 <mike.arbelaez@rice.edu>
2020-04-15 11:24:54 -0500

    Fix make_database_url tests and force setting of env vars for database config (#95)
    * remove db config in setup.cfg
    * force using environment variables for db
    * update make_database_url to not use defaults for environment variables
    * force settting DB_PASSWORD as an environment variable. Postgres image
    no longer allows for blank password.
    * raise EnvironmentError if mandatory_vars is not set.
    * remove get_config from conftest.py which used setup.cfg
    * fix make_database_url tests and a new one to check exception
    * update travis-compose.yml db service to use new env vars
    * add docker-compose ps command to ensure services are running in .travis.yml file
    * upgrade pytest-postgresql from 2.2.1 to 2.3.0
    * remove monkeypatch for test database name

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -8,24 +8,32 @@ URL_TEMPLATE = 'postgresql+psycopg2://{0}:{1}@{2}:{3}/{4}'
 
 
 @pytest.mark.parametrize('url_template,db_user,db_password,db_host,db_port,db_name',
-                         [(URL_TEMPLATE, 'leonardo', 'cowabunga', '127.0.0.1', 5432, 'pizza_db')])
+                         [(URL_TEMPLATE, 'leonardo', 'cowabunga', '127.0.0.1', '5432', 'pizza_db')])
 def test_make_database_url_heroku(url_template, db_user, db_password, db_host, db_port, db_name):
     test_url = url_template.format(db_user, db_password, db_host, db_port, db_name)
 
-    url = make_database_url()
-    assert not (url == test_url)
-
     os.environ['DATABASE_URL'] = test_url
+    os.environ['DB_USER'] = 'test_user'
+    os.environ['DB_PASSWORD'] = 'test_password'
+    os.environ['DB_HOST'] = 'test_host'
+    os.environ['DB_PORT'] = 'test_port'
+    os.environ['DB_NAME'] = 'test_name'
+
     url = make_database_url()
 
     assert url == test_url
 
 
 @pytest.mark.parametrize('url_template,db_user,db_password,db_host,db_port,db_name',
-                         [(URL_TEMPLATE, 'postgres', '', 'db', 5432, 'tests')])
-def test_make_database_url_default(url_template, db_user, db_password, db_host, db_port, db_name):
-
+                         [(URL_TEMPLATE, 'postgres', '', 'db', '5432', 'tests')])
+def test_make_database_url_with_env_vars(url_template, db_user, db_password, db_host, db_port,
+                                         db_name):
     os.environ['DATABASE_URL'] = ''
+    os.environ['DB_USER'] = db_user
+    os.environ['DB_PASSWORD'] = db_password
+    os.environ['DB_HOST'] = db_host
+    os.environ['DB_PORT'] = db_port
+    os.environ['DB_NAME'] = db_name
 
     test_url = url_template.format(db_user, db_password, db_host, db_port, db_name)
 
@@ -34,3 +42,16 @@ def test_make_database_url_default(url_template, db_user, db_password, db_host, 
     assert url == test_url
 
 
+@pytest.mark.parametrize('url_template,db_user,db_host,db_port,db_name',
+                         [(URL_TEMPLATE, 'postgres', 'db', '5432', 'tests')])
+def test_make_database_url_raise_environment_error(url_template, db_user, db_host, db_port,
+                                                   db_name):
+    os.environ['DATABASE_URL'] = ''
+    del os.environ['DB_PASSWORD']
+    os.environ['DB_USER'] = db_user
+    os.environ['DB_HOST'] = db_host
+    os.environ['DB_PORT'] = db_port
+    os.environ['DB_NAME'] = db_name
+
+    with pytest.raises(EnvironmentError):
+        assert make_database_url()
