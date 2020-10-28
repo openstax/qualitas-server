d7eecb3907e47cdd915f5d5c8f09cbaa974e9191
m1yag1 <mike.arbelaez@rice.edu>
2020-10-28 14:06:31 -0500

    Updated instanc/conf.py to use POSTGRES env vars
    * Application was setting it's own "custom" env vars for database
      specifics in order to create the db connection string. These were
      changed to mirror the ones set in the environment for postgres.
    * Removed usage of make_database_url and implemented a simple if
      statement.
    
    fixup: move postgresql env vars from main to database

--- instance/conf.py
+++ instance/conf.py
@@ -3,7 +3,6 @@ import uuid
 
 from qualitas.utils import make_database_url
 
-
 # SETTINGS
 
 # MAIN FLASK APPLICATION
@@ -13,25 +12,23 @@ SERVER_NAME = os.environ.get('FLASK_SERVER_NAME', None)
 PREFERRED_URL_SCHEME = os.environ.get('FLASK_PREFERRED_URL_SCHEME', 'http')
 
 # DATABASE
-DB_USER = os.environ.get('DB_USER', 'postgres')
-DB_PASSWORD = os.environ.get('DB_PASSWORD', '')
-DB_HOST = os.environ.get('DB_HOST', 'db')
-DB_PORT = os.environ.get('DB_PORT', '5432')
-DB_NAME = os.environ.get('DB_NAME', 'qualitas_db')
-DATABASE_URL = os.environ.get('DATABASE_URL', None)
-SQLALCHEMY_DATABASE_URI = make_database_url(
-    DB_USER=DB_USER,
-    DB_PASSWORD=DB_PASSWORD,
-    DB_HOST=DB_HOST,
-    DB_PORT=DB_PORT,
-    DB_NAME=DB_NAME,
-    DATABASE_URL=DATABASE_URL,
-)
+POSTGRES_USER = os.environ.get('POSTGRES_USER', 'qualitas')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'qualitas')
+POSTGRES_HOST = os.environ.get('POSTGRES_HOST', 'db')
+POSTGRES_PORT = os.environ.get('POSTGRES_PORT', '5432')
+POSTGRES_DB = os.environ.get('POSTGRES_DB', 'qualitas_db')
+DATABASE_URL = os.environ.get('DATABASE_URL', None)  # DATABASE URL SET BY HEROKU
+
+if DATABASE_URL:
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL
+else:
+    SQLALCHEMY_DATABASE_URI = (
+        f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}/{POSTGRES_DB}"
+    )
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # GITHUB
 GITHUB_USER = os.environ.get('GITHUB_USER', None)
-GITHUB_PASSWORD = os.environ.get('GITHUB_PASSWORD', None)
 GITHUB_CLIENT_ID = os.environ.get('GITHUB_CLIENT_ID', None)
 GITHUB_CLIENT_SECRET = os.environ.get('GITHUB_CLIENT_SECRET', None)
 GITHUB_AUTH_ENABLED = os.environ.get('GITHUB_AUTH_ENABLED', True)
